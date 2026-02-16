afe5a471c100e79d10293d3f642ad6f180aa6ecb
Charlie Thomson <charlie@thmsn.dev>
2026-02-16 15:27:56 -0500

    [patch] add valuable/serde features, derives

--- libwhich/src/error.rs
+++ libwhich/src/error.rs
@@ -1,6 +1,8 @@
 use thiserror::Error;
 
 #[derive(Debug, Clone, Error)]
+#[cfg_attr(feature = "serde", derive(serde::Serialize, serde::Deserialize))]
+#[cfg_attr(feature = "valuable", derive(valuable::Valuable))]
 pub enum WhichError {
     #[error("PATH is unset")]
     MissingPathVariable,
